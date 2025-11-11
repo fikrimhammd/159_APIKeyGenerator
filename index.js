f5b180cd8f093a1fed76333a3358c31cf439783b
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-11 16:06:32 +0700

    konek database

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ const port = 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// === Koneksi ke database ===
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Kendari2023',
+  database: 'db_apikey',
+  port: 3309
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('❌ Gagal konek ke database:', err);
+  } else {
+    console.log('✅ Terhubung ke database MySQL');
+  }
+});
+
+
